c556dfdbd2d34ec3a2c741f8f2147c2d55c42a43
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-18 21:16:33 +0900

    feat(Calculator): 커스텀 구분자, 계산식 저장과 계산 구현
    - addCustomSeparator(separator)
    - getCustomSeparators()
    - setExpression(expression)
    - getExpression()
    -sumExpression()
    
    CalculatorTest 통과

---
+++ src/calculator/Calculator.js
@@ -0,0 +1,68 @@
+import {
+  BANNED_CUSTOM_SEPARATOR,
+  INVALID_CUSTOM_SEPARATOR_LENGTH,
+  TOO_MANY_CUSTOM_SEPARATORS,
+  RESULT_OVERFLOW,
+  WRONG_EXPRESSION,
+} from '../const/errorMessages.js';
+import {
+  MAX_CUSTOM_SEPARATOR_COUNT,
+  MAX_CUSTOM_SEPARATOR_LENGTH,
+  MAX_RESULT_SAFE_INTEGER,
+} from '../const/limits.js';
+import DEFAULT_SEPARATORS from '../const/defaultSeparators.js';
+import StringUtils from '../string/StringUtils.js';
+
+export default class Calculator {
+  constructor() {
+    this.customSeparators = new Set();
+    this.expression = null;
+  }
+
+  addCustomSeparator(separator) {
+    if (this.customSeparators.size >= MAX_CUSTOM_SEPARATOR_COUNT)
+      throw new Error(TOO_MANY_CUSTOM_SEPARATORS);
+
+    if (separator.includes('//') || separator.includes('\\n') || /\d/.test(separator))
+      throw new Error(BANNED_CUSTOM_SEPARATOR);
+
+    if (separator.length > MAX_CUSTOM_SEPARATOR_LENGTH)
+      throw new Error(INVALID_CUSTOM_SEPARATOR_LENGTH);
+
+    this.customSeparators.add(separator);
+  }
+
+  getCustomSeparators() {
+    return this.customSeparators;
+  }
+
+  setExpression(expression) {
+    const separators = this.customSeparators.union(DEFAULT_SEPARATORS);
+    const regEx = StringUtils.getSplitRegEx(separators);
+
+    expression.split(regEx).forEach((e) => {
+      if (e === '' || Number.isNaN(+e)) throw new Error(WRONG_EXPRESSION);
+    });
+
+    this.expression = expression;
+  }
+
+  getExpression() {
+    return this.expression;
+  }
+
+  sumExpression() {
+    const separators = this.customSeparators.union(DEFAULT_SEPARATORS);
+    const regEx = StringUtils.getSplitRegEx(separators);
+    const numberArray = this.expression.split(regEx);
+
+    const result = numberArray
+      .map((n) => +n)
+      .reduce((acc, n) => {
+        if (acc + n <= MAX_RESULT_SAFE_INTEGER) return acc + n;
+        throw new Error(RESULT_OVERFLOW);
+      }, 0);
+
+    return result;
+  }
+}
