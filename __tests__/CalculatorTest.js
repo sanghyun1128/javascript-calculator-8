c34414675b97a2386feb2a4d50e2da1238af2956
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-18 22:24:18 +0900

    test(Calculator): 음수, 소수 관련 테스트 케이스 추가
    - 음수와 소수를 판별하기 위해 '-', '.' 커스텀 구분자 예외 처리
    - 음수가 있을 경우 에러
    - 소수의 길이제한 추가
    - 일반 소수 계산

--- __tests__/CalculatorTest.js
+++ __tests__/CalculatorTest.js
@@ -5,6 +5,8 @@ import {
   TOO_MANY_CUSTOM_SEPARATORS,
   WRONG_EXPRESSION,
   RESULT_OVERFLOW,
+  MINUS_IS_NOT_ALLOWED,
+  INVALID_SIGNIFICANT_DIGITS,
 } from '../src/const/errorMessages.js';
 import { MAX_CUSTOM_SEPARATOR_LENGTH, MAX_RESULT_SAFE_INTEGER } from '../src/const/limits.js';
 
@@ -28,7 +30,23 @@ describe('Calculator 테스트', () => {
   });
 
   test('\\n 포함한 커스텀 구분자 저장시 에러 발생', async () => {
-    const input = '\\n';
+    const input = 'u\\n';
+
+    const calculator = new Calculator();
+
+    expect(() => calculator.addCustomSeparator(input)).toThrow(BANNED_CUSTOM_SEPARATOR);
+  });
+
+  test('- 포함한 커스텀 구분자 저장시 에러 발생', async () => {
+    const input = '-d';
+
+    const calculator = new Calculator();
+
+    expect(() => calculator.addCustomSeparator(input)).toThrow(BANNED_CUSTOM_SEPARATOR);
+  });
+
+  test('. 포함한 커스텀 구분자 저장시 에러 발생', async () => {
+    const input = '. ';
 
     const calculator = new Calculator();
 
@@ -85,9 +103,35 @@ describe('Calculator 테스트', () => {
     expect(() => calculator.setExpression(input)).toThrow(WRONG_EXPRESSION);
   });
 
+  test('값에 음수가 있을 경우 에러 발생', async () => {
+    const input = `123,123,-123`;
+
+    const calculator = new Calculator();
+
+    expect(() => calculator.setExpression(input)).toThrow(MINUS_IS_NOT_ALLOWED);
+  });
+
+  test('숫자 자릿수가 너무 길 경우 에러 발생', async () => {
+    const input = `123,123,0.1231231231231231`;
+
+    const calculator = new Calculator();
+
+    expect(() => calculator.setExpression(input)).toThrow(INVALID_SIGNIFICANT_DIGITS);
+  });
+
   test('일반 계산', async () => {
     const input = '123,123,123';
     const output = 123 + 123 + 123;
+
+    const calculator = new Calculator();
+    calculator.setExpression(input);
+
+    expect(calculator.sumExpression()).toBe(output);
+  });
+
+  test('소수 계산', async () => {
+    const input = '123,0.123,123';
+    const output = 123 + 0.123 + 123;
 
     const calculator = new Calculator();
     calculator.setExpression(input);
