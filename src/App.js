ec5eeda4647bb3506bc4017a8e308da58ddba448
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-18 22:33:09 +0900

    feat(App): 프로그램 메인 로직 구현
    ApplicationTest 통과

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Calculator from './calculator/Calculator.js';
+import IOManager from './io/IOManager.js';
+import StringParser from './string/StringParser.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const calculator = new Calculator();
+      const userInput = await IOManager.getUserInput();
+
+      const customSeparatorDeclaration = StringParser.extractCustomSeparatorDeclaration(userInput);
+      const customSeparatorSet = StringParser.extractCustomSeparatorSet(customSeparatorDeclaration);
+      customSeparatorSet.forEach((e) => calculator.addCustomSeparator(e));
+      calculator.setExpression(StringParser.extractExpression(userInput));
+
+      IOManager.printResult(calculator.sumExpression());
+    } catch (error) {
+      error.message = IOManager.getFullErrorMessage(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
